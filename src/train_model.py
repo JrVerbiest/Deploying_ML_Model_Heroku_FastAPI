43a777d521287bbac220c546fa0259e1e6b274d8
Joeri Verbiest <jrv@jrverbiest.eu>
2022-01-08 15:46:07 +0100

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -1,15 +1,16 @@
-# Script to train machine learning model.
+""" Script to train machine learning model.
+
+"""
 
 from sklearn.model_selection import train_test_split
 
 from train_model.data import import_data, process_data
-# from train_model.model import train_model, compute_model_metrics, inference
+from train_model.model import train_model  # , compute_model_metrics, inference
 
 # load in the data.
 data = import_data("../data/census_cleaned.csv")
 
-# Optional enhancement,
-# use K-fold cross validation instead of a train-test split.
+# split data
 train, test = train_test_split(data, test_size=0.20)
 
 cat_features = [
@@ -26,6 +27,4 @@ X_train, y_train, encoder, lb = process_data(
     train, categorical_features=cat_features, label="salary", training=True
 )
 
-# Proces the test data with the process_data function.
-
-# Train and save a model.
+model = train_model(X_train, y_train)
