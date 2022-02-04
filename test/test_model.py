74998059425c3164165d501ee704f32ce9c21f24
J. R. Verbiest <39417102+JrVerbiest@users.noreply.github.com>
2022-02-04 11:35:12 +0100

    add all test to test_model

--- test/test_model.py
+++ test/test_model.py
@@ -2,16 +2,16 @@ import sys
 sys.path.insert(0, './src')
 sys.path.insert(0, './src/train_model')
 
-import pytest
+import numpy as np
+
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 from tm_helper import load_pkl
 from data import import_data, process_data
-from model import train_model, inference
+from model import train_model, inference, compute_model_metrics
 
 
-@pytest.fixture
 def test_lr_model():
     """test model
 
@@ -46,3 +46,36 @@ def test_prediction():
 
     model = train_model(X_train, y_train)
     pred = inference(model, X_test)
+
+    assert isinstance(pred, (np.ndarray, np.generic))
+
+
+def test_compute_model_metrics():
+    """test if the metrics fall between 0 and 1
+    """
+
+    data = import_data("./data/clean_census.csv")
+    
+    train, test = train_test_split(data, test_size=0.20)
+
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+
+    X_train, y_train, encoder_train, lb_train = process_data(train, categorical_features=cat_features, label="salary", training=True)   
+    X_test, y_test, encoder_test, lb_test = process_data(test, categorical_features=cat_features, label="salary", training=False, encoder=encoder_train, lb=lb_train)
+
+    model = train_model(X_train, y_train)
+    pred = inference(model, X_test)
+    precision, recall, fbeta = compute_model_metrics(y_test, pred)
+
+    assert 0 <= precision <= 1
+    assert 0 <= recall <= 1
+    assert 0 <= fbeta <= 1
