e5facc58faa6d4641dfb32c8765ced5c6e85225f
J. R. Verbiest <39417102+JrVerbiest@users.noreply.github.com>
2022-02-04 13:56:42 +0100

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -6,8 +6,6 @@ import sys
 sys.path.insert(0, "./src/")
 sys.path.insert(1, "./train_model")
 
-print(sys.path)
-
 from sklearn.model_selection import train_test_split
 
 from data import import_data, process_data
@@ -17,32 +15,43 @@ from tm_helper import save_pkl
 # parameters
 PATHCLEANDATA = "../data/clean_census.csv"
 PATHMODEL = "../model/lr_model.pkl"
+PATHENCODER = "../model/lr_encoder.pkl"
+PATHLB = "../model/lr_lb.pkl"
 
-# load in the data.
-data = import_data(PATHCLEANDATA)
 
-# split data
-train, test = train_test_split(data, test_size=0.20)
+def run():
 
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
 
-X_train, y_train, encoder_train, lb_train = process_data(train, categorical_features=cat_features, label="salary", training=True)
-X_test, y_test, encoder_test, lb_test = process_data(test, categorical_features=cat_features, label="salary", training=False, encoder=encoder_train, lb=lb_train)
+    # load in the data.
+    data = import_data(PATHCLEANDATA)
 
-model = train_model(X_train, y_train)
+    # split data
+    train, test = train_test_split(data, test_size=0.20)
+    
+    X_train, y_train, encoder_train, lb_train = process_data(train, categorical_features=cat_features, label="salary", training=True)
+    X_test, y_test, _, _ = process_data(test, categorical_features=cat_features, label="salary", training=False, encoder=encoder_train, lb=lb_train)
 
-save_pkl(model, PATHMODEL)
+    model = train_model(X_train, y_train)
 
-pred = inference(model, X_test)
-precision, recall, fbeta = compute_model_metrics(y_test, pred)
+    save_pkl(model, PATHMODEL)
+    save_pkl(encoder_train, PATHENCODER)
+    save_pkl(lb_train, PATHLB)
 
-print("Metrics: precision = %s,  recall = %s and fbeta = %s" %(round(precision,2), round(recall,2), round(fbeta,2)))
+    pred = inference(model, X_test)
+    precision, recall, fbeta = compute_model_metrics(y_test, pred)
+
+    print("Metrics: precision = %s,  recall = %s and fbeta = %s" %(round(precision,2), round(recall,2), round(fbeta,2)))
+
+
+if __name__ == '__main__':
+    run()
